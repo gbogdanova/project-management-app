dd3ef063b8037553247d4121e9845114b4ef1ad4
gbogdanova <gaaalo44ka@gmail.com>
2025-01-15 14:53:21 -0800

    feat: handle add project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
+import { useState } from 'react'
+
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject(){
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProject: null,
+      }
+    })
+  }
+  function handleAddProject(projectData){
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      }
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+  let content;
+
+  if(projectState.selectedProject === null){
+    content = <NewProject  onAdd={handleAddProject}/>
+  } else if (projectState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+console.log(projectState);
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NewProject />
-      <NoProjectSelected />
+      <Sidebar  onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
