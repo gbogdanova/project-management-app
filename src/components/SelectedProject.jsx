02cc450ea735bdce336e0407c8480b5d37946921
gbogdanova <gaaalo44ka@gmail.com>
2025-01-17 11:14:44 -0800

    feat: add Tasks component

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tasks from './Tasks'
 
 export default function SelectedProject({project, onDelete}) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
@@ -16,7 +17,7 @@ export default function SelectedProject({project, onDelete}) {
         <p className="mb-4 text-stone-400">{formattedDate}</p>
         <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
       </header>
-      Tasks
+      <Tasks />
     </div>
   )
 }
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NewTask from './NewTask'
+
+export default function Tasks() {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold test-stone-700 mb-4">TASKS</h2>
+      <NewTask />
+      <p className="text-stone-800 my-4">This project does not have any tasks yet.</p>
+      <ul>
+      </ul>
+    </section>
+  )
+}
